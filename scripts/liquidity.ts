e516e2ff1c619d76fa30fe3c3dc3c5a86c38ae34
Wang Shuaishuai <shuaishuai.wan@lightnet.io>
2022-01-06 17:33:51 +0800

    add liquidity

--- scripts/liquidity.ts
+++ scripts/liquidity.ts
@@ -5,6 +5,7 @@
 // Runtime Environment's members available in the global scope.
 import { ethers } from "hardhat";
 import { Contract } from 'ethers'
+import { MaxUint256 } from '@ethersproject/constants'
 import UniswapV2Router02 from '../artifacts/contracts/UniswapV2Router02.sol/UniswapV2Router02.json'
 
 
@@ -15,8 +16,11 @@ async function main() {
   const router02Address="0xdC1037F6d3d148d31F8924ec86e06Bd225362Fe6"
   const router02 = new Contract(router02Address, JSON.stringify(UniswapV2Router02.abi),deployer)
  
-  const liquidity = await router02.addLiquidity()
-  console.log("UniswapV2Router02 MINIMUM_LIQUIDITY:", liquidity);//1000
+  const [amountA,amountB,liquidity] = await router02.addLiquidity("0x5FbDB2315678afecb367f032d93F642f64180aa3","0xf0b221f97E263BbE5843466ee3b7247eeD9e0DA9",10000000,100000000,0,0,deployer.getAddress(),MaxUint256)
+  
+  console.log("amountA:", amountA);
+  console.log("amountB:", amountB);
+  console.log("liquidity:", liquidity);
  
 }
 
